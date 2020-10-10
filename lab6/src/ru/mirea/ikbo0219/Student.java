d54e4aa72b09fb46afb028ce4e3dfdf366e5599b
V31dy <71170989+V31dy@users.noreply.github.com>
2020-10-10 14:47:43 +0300

    Add files via upload

---
+++ lab6/src/ru/mirea/ikbo0219/Student.java
@@ -0,0 +1,14 @@
+package ru.mirea.ikbo0219;
+
+public class Student {
+    private int id;
+    Student(int id)
+    {
+        this.id=id;
+    }
+
+    public int getId()
+    {
+        return id;
+    }
+}
