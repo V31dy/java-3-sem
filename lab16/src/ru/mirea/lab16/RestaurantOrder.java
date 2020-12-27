ac8d59434456d0ed9b34cddcc00f96b983ae0ad7
V31dy <71170989+V31dy@users.noreply.github.com>
2020-12-27 13:19:41 +0300

    Add files via upload

---
+++ lab16/src/ru/mirea/lab16/RestaurantOrder.java
@@ -0,0 +1,11 @@
+package ru.mirea.pr16;
+
+import java.util.Collection;
+
+public class RestaurantOrder extends OrderManager {
+    public RestaurantOrder() {}
+
+    public RestaurantOrder(Collection<Item> collection) {
+        super(collection);
+    }
+}
